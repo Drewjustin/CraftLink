f09bdb64ea294683817f822a2cdb7d50546fdd28
georgepaulsen <georgemartinpaulsen@users.noreply.github.com>
2019-11-27 03:14:35 -0500

    cleaned up supplier.php
    Made some changes to how form adds a new product. However, it is still unable to show the table and claims the query has no entries. I am not sure what to do

--- supplier.php
+++ supplier.php
@@ -12,19 +12,35 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = 'SELECT `product_id`, `product_name`, `product_dscpt`, `product_unitinWhichSold` `product_inStock` FROM CraftLink';
-$result = $conn->query($sql);
+//Grab everything from the craflink table
+$sql = "SELECT * FROM `CraftLink`";
 
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-   if (isset($_POST["Submit"]) && $_POST['Submit'] =='Submit'){
-      // var_dump($_POST);
-      $sql2 = 'INSERT INTO `CraftLink` (`product_name`, `product_dscpt`,
-      `product_price`, `product_unitinWhichSold`) VALUES' . '('. $_POST['name'] . ',' . $_POST['description']
-      . $_POST['price'] . ',' . $_POST['unit_sold'] . ')';
-      $result2 = $conn->query($sql2);
+// if($_SERVER['REQUEST_METHOD'] == 'POST'){
+//    if (isset($_POST["Submit"]) && $_POST['Submit'] =='Submit'){
+//       // var_dump($_POST);
+//       $sql2 = 'INSERT INTO `CraftLink` (`product_name`, `product_dscpt`,
+//       `product_price`, `product_unitinWhichSold`) VALUES' . '('. $_POST['name'] . ',' . $_POST['description']
+//       . $_POST['price'] . ',' . $_POST['unit_sold'] . ')';
+//       $result2 = $conn->query($sql2);
+//    }
+// }
+
+// ADD PRODUCT BUTTON
+$resultAddP = NULL;
+if(isset($_POST['addProduct'])){
+   $addProduct = $_POST['addProduct'];
+   if($addProduct){
+      $name = $_POST['name'];
+      $dscpt = $_POST['description'];
+      $price = $_POST['price'];
+      $unit = $_POST['unit_sold'];
+      $sqlAddP = "INSERT INTO `CraftLink` (`product_name`, `product_dscpt`, `product_unitinWhichSold`)
+      VALUES ($name, $dscpt, $price, $unit)";
+      $resultAddP = $conn->query($sqlAddP);
    }
 }
+$result = $conn->query($sql);
 
  ?>
 
@@ -40,28 +56,28 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <body>
        <section class="main">
           <h1>Brewer Home</h1>
-
           <section id="product_table">
              <?php
              if ($result->num_rows > 0) {
-                 echo "<table><tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Unit Sold</th><th>In Stock</th>";
+                 echo "<table>
+                 <tr>
+                 <th>ID</th>
+                 <th>Name</th>
+                 <th>Description</th>
+                 <th>Price</th>
+                 <th>Unit Sold</th>
+                 <th>In Stock</th>";
                  // output data of each row
                  while($row = $result->fetch_assoc()) {
-                     echo "<tr><td>"
-                     . $row["product_id"]
-                     . "</td><td>"
-                     . $row["product_name"]
-                     . "</td><td>"
-                     . $row["product_dscpt"]
-                     . "</td><td>"
-                     . $row["product_price"]
-                     . "</td><td>"
-                     . $row["product_unit"]
-                     . "</td><td>"
-                     . $row["product_inStock"]
-                     . "</td><td>"
-                     . "<a href='edit.php?'>edit</a>;"
-                     . "</td><td></tr>";
+                     echo "<tr>"
+                     . "<td>" . $row["product_id"] . "</td>"
+                     . "<td>" . $row["product_name"] . "</td>"
+                     . "<td>" . $row["product_dscpt"] . "</td>"
+                     . "<td>" . $row["product_price"] . "</td>"
+                     . "<td>" . $row["product_unit"] . "</td>"
+                     . "<td>" . $row["product_inStock"] . "</td>"
+                     // . "<a href='edit.php?'>edit</a>;"
+                     . "</tr>";
                  }
                  echo "</table>";
              } else {
@@ -72,11 +88,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             <article id="add_products">
                <button type="button" id="add_product_button">Add Product</button>
                <form id="add_product" action="supplier.php" method="post">
-                  Name:<input type="text" id='p_name' name="name" value="">
-                  Description:<input type="text" id='p_dscpt' name="description" value="">
-                  Price:<input type="int" id='p_price' name="price" value="">
-                  Unit Sold:<input type="text" id='p_unit'name="unit_sold" value="">
-                  <input type="submit" name="Submit" value="Submit">
+                  Name:<input type="text" name="name" value="">
+                  Description:<input type="text" name="description" value="">
+                  Price:<input type="int"  name="price" value="">
+                  Unit Sold:<input type="text" name="unit_sold" value="">
+                  <input type="submit" name="addProduct" value="Submit">
                </form>
             </article>
           <?php
